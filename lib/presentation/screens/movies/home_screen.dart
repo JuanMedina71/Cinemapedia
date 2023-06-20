9b97c20b14d0e4479eb2854cef8ee3039fffa6ce
Juan Carlos Medina <carlos.medina@uadec.edu.mx>
2023-06-20 10:16:37 -0600

    First commit Cinemapedia

---
+++ lib/presentation/screens/movies/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+
+  // Ruta que se utilizara con Go Router
+  static const name = 'home-screen';
+
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Placeholder(),
+    );
+  }
+}
