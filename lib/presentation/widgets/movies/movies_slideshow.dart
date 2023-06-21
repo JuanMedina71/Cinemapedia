e1153ae53b9d6b7f0e8a64ef2d3d7c3a197e427f
Juan Carlos Medina <carlos.medina@uadec.edu.mx>
2023-06-21 13:56:52 -0600

    SlideBar finish

--- lib/presentation/widgets/movies/movies_slideshow.dart
+++ lib/presentation/widgets/movies/movies_slideshow.dart
@@ -1,32 +1,40 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import '../../../domain/entities/movie.dart';
 import 'package:card_swiper/card_swiper.dart';
 
 class MoviesSlidesShow extends StatelessWidget {
-
   final List<Movie> movies;
-  
 
   const MoviesSlidesShow({super.key, required this.movies});
 
-
-
   @override
+
+
+
   Widget build(BuildContext context) {
+
+  final colors = Theme.of(context).colorScheme;
     return SizedBox(
       height: 210,
       width: double.infinity,
       child: Swiper(
         viewportFraction: 0.8,
         scale: 0.85,
-        itemCount: movies.length,
-        itemBuilder: (context, index) => _Slide(movie: movies[index]), 
+        autoplay: true,
+        pagination: SwiperPagination(
+          margin: const EdgeInsets.only(top: 0),
+          builder: DotSwiperPaginationBuilder(
+            activeColor: colors.primary,
+            color: colors.secondary
+          )
         ),
+        itemCount: movies.length,
+        itemBuilder: (context, index) => _Slide(movie: movies[index]),
+      ),
     );
   }
 }
-
-
 
 class _Slide extends StatelessWidget {
   final Movie movie;
@@ -35,6 +43,28 @@ class _Slide extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final decoration = BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: const [
+          BoxShadow(
+              color: Colors.black45, blurRadius: 20, offset: Offset(0, 10))
+        ]);
+
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30),
+      child: DecoratedBox(
+          decoration: decoration,
+          child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(movie.backdropPath, fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) {
+                if (loadingProgress != null) {
+                  return const DecoratedBox(
+                      decoration: BoxDecoration(color: Colors.black12));
+                }
+
+                return FadeIn(child: child);
+              }))),
+    );
   }
 }
