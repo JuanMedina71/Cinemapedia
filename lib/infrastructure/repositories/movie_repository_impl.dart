5db2e9a337f091edb999724f4d95e4c014047cd1
Juan Carlos Medina <carlos.medina@uadec.edu.mx>
2023-06-21 11:46:55 -0600

    Listado de peliculas en pantalla

--- lib/infrastructure/repositories/movie_repository_impl.dart
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -1,7 +1,6 @@
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/domain/repositories/movies_repository.dart';
-import 'package:flutter/material.dart';
 
 
 class MovieRepositoryImpl extends MoviesRepository {
